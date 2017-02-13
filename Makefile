5a91bd61f299e1a24251f38e3ee2202690cd5b7d
Gennady Kovshenin <gennady@kovshenin.com>
2017-02-14 03:43:53 +0500

    Getting ready to draw something

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 P=c8
 CFLAGS=-std=gnu11 -O0 -Wall -Werror -g
 CC=gcc
+ROM=roms/PONG
 
 $(P): $(OBJECT)
+
+run: $(P)
+	./$(P) $(ROM)
+
+test: $(P)
+	TEST=1 ./$(P)
--- c8.c
+++ c8.c
@@ -11,17 +11,17 @@
 /**
  * An 16-bit instruction.
  */
-typedef int16_t c8_instruction_t;
+typedef uint16_t c8_instruction_t;
 
 /**
  * An 16-bit address.
  */
-typedef int16_t c8_address_t;
+typedef uint16_t c8_address_t;
 
 /**
  * A 16-bit register.
  */
-typedef int16_t c8_register_t;
+typedef uint8_t c8_register_t;
 
 /**
  * The ROM.
@@ -34,6 +34,16 @@ typedef struct {
 } ROM_t;
 
 /**
+ * The display.
+ */
+typedef struct {
+	/**
+	 * A 64x32 bitfield of pixels.
+	 */
+	uint64_t p[32];
+} Display_t;
+
+/**
  * The CPU.
  */
 typedef struct {
@@ -41,10 +51,26 @@ typedef struct {
 	 * The program counter.
 	 */
 	c8_address_t pc;
+
 	/**
 	 * The 16 registers.
 	 */
 	c8_register_t v[16];
+
+	/**
+	 * The I 16-bit register.
+	 */
+	c8_address_t i;
+
+	/**
+	 * A pointer to the ROM to read data, etc.
+	 */
+	ROM_t *rom;
+
+	/**
+	 * A pointer to the display.
+	 */
+	Display_t *display;
 } CPU_t;
 
 /**
@@ -61,6 +87,19 @@ c8_instruction_t rom_get(ROM_t *rom, c8_address_t address) {
 }
 
 /**
+ * Retrieve one byte of data from ROM.
+ *
+ * \param rom The ROM to use.
+ * \param address The address to get the data from.
+ *
+ * \return uint8_t The byte retrieved.
+ */
+uint8_t rom_read(ROM_t *rom, c8_address_t address) {
+	fseek(rom->file, address, SEEK_SET);
+	return fgetc(rom->file);
+}
+
+/**
  * Reset the state of this CPU.
  *
  * \param cpu This CPU.
@@ -72,6 +111,7 @@ void cpu_reset(CPU_t *cpu) {
 	for (int i = 0; i < 16; i++) {
 		cpu->v[i] = 0;
 	}
+	cpu->i = 0;
 }
 
 /**
@@ -88,6 +128,44 @@ void cpu_dump(CPU_t *cpu) {
 }
 
 /**
+ * Dump the state of the display.
+ *
+ * \param display The display to dump.
+ *
+ * \return void
+ */
+void display_dump(Display_t *display) {
+	/** Frames */
+	for (int f = 0; f < 64; f++)
+		printf("-");
+
+	for (int y = 0; y < 32; y++) {
+		printf("\n|");
+		int p = display->p[y];
+		for (int x = 0; x < 64; x++) {
+			printf("%c", p & 0x1 ? '*' : ' ');
+			p >>= 1;
+		}
+		printf("|\n");
+	}
+
+	for (int f = 0; f < 64; f++)
+		printf("-");
+}
+
+/**
+ * Clear the display.
+ *
+ * \param display The display to clear.
+ *
+ * \return void
+ */
+void display_clear(Display_t *display) {
+	for (int p = 0; p < 32; p++)
+		display->p[p] = 0;
+}
+
+/**
  * Execute an instruction.
  *
  * \param cpu The CPU to execute the instruction on.
@@ -96,10 +174,27 @@ void cpu_dump(CPU_t *cpu) {
  * \return void
  */
 void cpu_execute(CPU_t *cpu, c8_instruction_t instruction) {
-	if (/* 6XNN */ instruction >> 12 == 0x6 /* Set VX to NN */) {	
+	       if (/* 6XNN */ ((instruction >> 12) & 0xf) == 0x6 /* Set VX to NN */) {	
 		cpu->v[instruction >> 8 & 0xf] = instruction & 0xff;
+
+	} else if (/* ANNN */ ((instruction >> 12) & 0xf) == 0xa /* Set I to NNN */) {
+		cpu->i = instruction & 0xfff;
+	
+	} else if (/* DXYN */ ((instruction >> 12) & 0xf) == 0xd /* Draw 8xN sprite at VX VY, set VF to screen set */) {
+
+		uint8_t x = cpu->v[(instruction >> 8) & 0xf];
+		uint8_t y = cpu->v[(instruction & 0xf0) >> 4];
+		for (uint8_t h = 0; h < (instruction & 0xf); h++) {
+			uint8_t data = rom_read(cpu->rom, cpu->i - 0x200 + h);
+
+			/** \todo figure this out */
+			printf("put %0x at %0x %0x\n", data, x, y - h);
+		}
+
+		// printf("wants to draw sprite of height %0x from memory %04x at %04x, %04x\n", h & 0xf, cpu->i - 0x200, cpu->v[(instruction >> 8) & 0xf], cpu->v[(instruction & 0xf0) >> 4]);
+		// display_dump(cpu->display);
 	} else {
-		fprintf(stderr, "Unknown instruction %04x!\n", instruction & 0xffff);
+		fprintf(stderr, "Unknown instruction %04x!\n", instruction);
 		exit(-1);
 	}
 
@@ -143,6 +238,12 @@ int main(int argc, char *argv[]) {
 	ROM_t rom;
 	rom.file = fopen(argv[1], "rb");
 
+	Display_t display;
+	display_clear(&display);
+
+	cpu.rom = &rom;
+	cpu.display = &display;
+
 	while (true) {
 		cpu_execute(&cpu, rom_get(&rom, cpu.pc));
 	}
@@ -161,11 +262,27 @@ int test(int argc, char *argv[]) {
 	CPU_t cpu;
 	cpu_reset(&cpu);
 
-	TEST_EQUALS(cpu.pc, 0);
+	TEST_EQUALS(cpu.pc, 0)
+	for (int i = 0; i < 16; i++)
+		TEST_EQUALS(cpu.v[i], 0)
+	TEST_EQUALS(cpu.i, 0)
+
+	/** 6VNN Set V to NN */
 	cpu_execute(&cpu, 0x6001); TEST_EQUALS(cpu.v[0], 0x1)
 	cpu_execute(&cpu, 0x6a9f); TEST_EQUALS(cpu.v[0xa], 0x9f)
 	cpu_execute(&cpu, 0x6fff); TEST_EQUALS(cpu.v[0xf], 0xff)
 	TEST_EQUALS(cpu.pc, 6)
+
+	/** ANNN Set I to NNN */
+	cpu_execute(&cpu, 0xa423); TEST_EQUALS(cpu.i, 0x423)
+	cpu_execute(&cpu, 0xa0ff); TEST_EQUALS(cpu.i, 0x0ff)
+
+	/**
+	 * DXYN
+	 * 
+	 * Draw a sprite at position VX, VY with N bytes of sprite data starting at the address stored in I
+	 * Set VF to 01 if any set pixels are changed to unset, and 00 otherwise!
+	 */
 
 	printf("\n%d tests: %d passed, %d failed\n", passed + failed, passed, failed);
 
